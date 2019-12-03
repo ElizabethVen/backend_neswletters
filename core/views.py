cbbda886c0e3ad8307ccfaa06fc8236f466c84c1
Elizabeth Ventura <elizabethventuramirez@gmail.com>
2019-12-03 17:00:01 -0600

    Agregamos endpoint de usuarios

---
+++ core/views.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+
+from core.serializers import UserSerializer, CreateUserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+        Regresa la instancia de un usuario
+    list:
+        Regresa la lista de usuarios
+    update:
+        Actualiza un usuario
+    partial_update:
+        Actualiza un campo en particular de un usuario
+    delete:
+        Elimina un usuario
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes =[AllowAny]
+        return[permission() for permission in self.permission_classes]
+
+
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return CreateUserSerializer
+        return UserSerializer
+
