db037cf59bc458f701c66fbd40f7a2443c93a21a
linux-kbl <edgardo.torres@hotmail.comls>
2019-12-06 11:11:59 -0600

    ADD test user sin token

--- core/tests/auth/test_authentication.py
+++ core/tests/auth/test_authentication.py
@@ -4,13 +4,19 @@ from rest_framework.test import APITestCase
 
 
 class AuthenticationTestCase(APITestCase):
+    """
+    encapsula las reglas de autenticación
+    """
 
     def setUp(self):
+        """
+        crea un usaurio que se guarda en la variable user
+        """
         self.user = User.objects.create_user('test_user', 'test_user@gmail.com', '123456')
 
     def test_authenticate(self):
         """
-        regresa un token
+        manda un post y tiene que regresa un token
         """
         result = self.client.post('/api/token/', {'username': 'test_user', 'password': '123456'})
 
@@ -19,7 +25,7 @@ class AuthenticationTestCase(APITestCase):
 
     def test_valid_token(self):
         """
-        el token es valido
+        valida el token
         """
         result = self.client.post('/api/token/', {'username': 'test_user', 'password': '123456'})
         token = result.data['access']
